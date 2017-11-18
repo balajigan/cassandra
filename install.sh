063efaeda05512e4a342c45205760bdc982c2a52
balajigan <balaji_inf@yahoo.com>
2017-11-18 08:53:06 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-# install 
+# Step1: Instal JDK:
 
+sudo apt-get install -y openjdk-8-jdk
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+
+export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
+
+## Step2: Install & Start cassandra
+
+cd /opt
+
+mkdir cassandra
+
+cd cassandra
+
+curl -O -k http://apache.claz.org/cassandra/3.11.1/apache-cassandra-3.11.1-bin.tar.gz
+
+tar -xvzf apache-cassandra-3.11.1-bin.tar.gz
+
+bin/cassandra -R
