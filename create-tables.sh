0b0d255c11138a46786d2217003bf4039b43b323
balajigan <balaji_inf@yahoo.com>
2017-12-04 14:00:52 -0600

    Update create-tables.sh

--- create-tables.sh
+++ create-tables.sh
@@ -1,10 +1,6 @@
 
+CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {'class' : 'NetworkTopologyStrategy','replication_factor' : 1};
+  
 
-CREATE TABLE orders (
-  orderId text,
-  fname text,
-  lname text,
-  PRIMARY KEY (uid) 
-) WITH compaction= { 'class': 'MemoryOnlyStrategy' }
-     AND caching = {'keys':'NONE', 'rows_per_partition':'NONE'};
+CREATE TABLE orders (orderId text, prdId text, orderDesc text, orderQty int, PRIMARY KEY (uid) ) WITH compaction= { 'class': 'MemoryOnlyStrategy' } AND caching = {'keys':'NONE', 'rows_per_partition':'NONE'};
      
