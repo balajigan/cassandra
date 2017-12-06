b1ffd100290be7784eadab9d36d61de7eb9cde6c
root <root@instance-1.c.first-project-181415.internal>
2017-12-06 02:35:15 +0000

    Made some chnages

--- cassandra-client/src/main/java/com/batria/App.java
+++ cassandra-client/src/main/java/com/batria/App.java
@@ -1,5 +1,14 @@
 package com.batria;
 
+
+import com.datastax.driver.core.Session;
+import org.codehaus.jackson.map.ObjectMapper;
+import java.io.IOException;
+import com.google.gson.Gson;
+import com.batria.Connection;
+import org.json.JSONObject;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 /**
  * Hello world!
  *
@@ -9,5 +18,27 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	Connection conn = null;
+	Session session = null;
+      try{
+        conn = new Connection();
+	session = conn.getSession();
+
+//	JSONObject jsonObj = new JSONObject();
+
+	ResultSet rs = session.execute("SELECT JSON * FROM test.orders");
+	Row row = rs.one();
+	String jsonString = row.getString(0);
+
+	System.out.println(jsonString);
+       }
+      catch(Exception ex)
+      {
+	      System.out.println("Exception !!!");
+      }
+      finally
+      {
+	      conn.close();
+      }
     }
 }
