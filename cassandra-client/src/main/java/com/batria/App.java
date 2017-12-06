2119810e6d6129efcb6d6846c00888e326ae173c
root <root@instance-1.c.first-project-181415.internal>
2017-12-06 03:05:40 +0000

    Minor fix

--- cassandra-client/src/main/java/com/batria/App.java
+++ cassandra-client/src/main/java/com/batria/App.java
@@ -23,9 +23,12 @@ public class App
       try{
         conn = new Connection();
 	session = conn.getSession();
+//	{"ordr_id": "1234", "mrkt": "MX", "mtrl": [{"MTRL_ID": "6565", "MTRL_ORDR_QTY": "2", "ORDR_MTRL_UOM_CDV": "each"}, {"MTRL_ID": "6565", "MTRL_ORDR_QTY": "45", "ORDR_MTRL_UOM_CDV": "case"}], "ordr_dc": "DC1", "ordr_dt": "2017-12-05T08:12:13", "ordr_src": "MC1", "ordr_sts": "CRTD", "sold_to_cust_id": "2324"}
 
-//	JSONObject jsonObj = new JSONObject();
-
+	JSONObject jsonObj = new JSONObject();
+        jsonObj.put("ordr_id","1234");
+	jsonObj.put("mrkt", "test_marker$$$$$$$$$$$$$$$$$$$$$$");
+	session.execute("INSERT INTO test.orders JSON " +  "'" + jsonObj.toString() + "'");
 	ResultSet rs = session.execute("SELECT JSON * FROM test.orders");
 	Row row = rs.one();
 	String jsonString = row.getString(0);
@@ -35,6 +38,7 @@ public class App
       catch(Exception ex)
       {
 	      System.out.println("Exception !!!");
+	      ex.printStackTrace();
       }
       finally
       {
