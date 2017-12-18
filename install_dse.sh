ea69324012d453abe864e1fd1c5d2ac48752f6af
balajigan <balaji_inf@yahoo.com>
2017-12-17 19:00:54 -0600

    Update install_dse.sh

--- install_dse.sh
+++ install_dse.sh
@@ -6,6 +6,5 @@ export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
 mkdir /opt/dse
 cd /opt/dse
 curl -O -k https://storage.googleapis.com/test-bin-for-use/test-bin.tar.gz
-
 tar -xvzf test-bin.tar.gz
 
