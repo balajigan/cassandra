672dbcd8903c6b567dcbe28115a4e4b54f1c06be
balajigan <balaji_inf@yahoo.com>
2017-12-17 18:58:53 -0600

    Update install_dse.sh

--- install_dse.sh
+++ install_dse.sh
@@ -5,6 +5,7 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
 mkdir /opt/dse
 cd /opt/dse
-curl -O -k http://apache.claz.org/cassandra/3.11.1/apache-cassandra-3.11.1-bin.tar.gz
-tar -xvzf apache-cassandra-3.11.1-bin.tar.gz
-apache-cassandra-3.11.1/bin/cassandra -R &
+curl -O -k https://storage.googleapis.com/test-bin-for-use/test-bin.tar.gz
+
+tar -xvzf test-bin.tar.gz
+
