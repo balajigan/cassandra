cc3f30d97ff7118824c4b1985834dc70c9b90916
balajigan <balaji_inf@yahoo.com>
2017-12-21 23:07:08 -0600

    Update HttpsHandle.java

--- cassandra-client/src/main/java/com/batria/HttpsHandle.java
+++ cassandra-client/src/main/java/com/batria/HttpsHandle.java
@@ -1,6 +1,19 @@
 
 package com.batria;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.InputStreamBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.util.EntityUtils;
+
 
 public class HttpsHandle
 {
@@ -13,7 +26,39 @@ public class HttpsHandle
         public void start()
 	{
 
-
+		// the file we want to upload
+		File inFile = new File("table1_1.csv");
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(inFile);
+			DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+			
+			// server back-end URL
+			HttpPost httppost = new HttpPost("http://localhost:8080/hazelcast/upload");
+			MultipartEntity entity = new MultipartEntity();
+			// set the file input stream and file name as arguments
+			entity.addPart("file", new InputStreamBody(fis, inFile.getName()));
+			httppost.setEntity(entity);
+			// execute the request
+			HttpResponse response = httpclient.execute(httppost);
+			
+			int statusCode = response.getStatusLine().getStatusCode();
+			HttpEntity responseEntity = response.getEntity();
+			String responseString = EntityUtils.toString(responseEntity, "UTF-8");
+			
+			System.out.println("[" + statusCode + "] " + responseString);
+			
+		} catch (ClientProtocolException e) {
+			System.err.println("Unable to make connection");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Unable to read file");
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fis != null) fis.close();
+			} catch (IOException e) {}
+		}
 
 	}
 
